fe46bb8bb789452388ff40990528bb6cefe0860c
Evgen Sumskyi <grendersq12@gmail.com>
2025-04-29 17:41:06 +0300

    test

--- transport_settings/settings.py
+++ transport_settings/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["*"]
+
 
 # Application definition
 
