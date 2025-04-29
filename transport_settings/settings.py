1cd41fd9f87e1010541e83f6b8fa8fe88c3726dd
Evgen Sumskyi <grendersq12@gmail.com>
2025-04-29 17:43:36 +0300

    test

--- transport_settings/settings.py
+++ transport_settings/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["py-transport-api.onrender.com"]
 
 
 # Application definition
